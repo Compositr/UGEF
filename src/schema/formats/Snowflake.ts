586cd7f920d4a8b563c27b1a768340f7b866540b
Jim <compositr@compositr.dev>
2022-08-21 07:33:32 +1000

    fix: regex for snowflakes
    Signed-off-by: Jim <compositr@compositr.dev>

--- src/schema/formats/Snowflake.ts
+++ src/schema/formats/Snowflake.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
  * @description Regex matching a Twitter Snowflake
  * @since v0.0.1
  */
-export const UnwrappedSnowflake = z.string().regex(/^[0-9]$/);
+export const UnwrappedSnowflake = z.string().regex(/^[0-9]{7,}$/);
 
 export const SnowflakeObject = z.object({
   id: UnwrappedSnowflake,
