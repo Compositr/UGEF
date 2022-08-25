96d720c9924b62a37fe50b34f856cba7a3d9d860
Jim <compositr@compositr.dev>
2022-08-25 21:49:03 +1000

    fix: include whole path
    Signed-off-by: Jim <compositr@compositr.dev>

--- src/common/utility/validationErrorToString.ts
+++ src/common/utility/validationErrorToString.ts
@@ -1,5 +1,5 @@
 import { ZodError } from "zod";
 
 export default function (error: ZodError) {
-  return error.issues.map((e) => `${e.message} ${e.path}`).join(", ");
+  return error.issues.map((e) => `${e.code}: ${e.message} ${e.path.join(" ")}`).join(", ");
 }
