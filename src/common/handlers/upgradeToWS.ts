110b86cadedda2b05173226a4ea8ad93107d2a30
Jim <compositr@compositr.dev>
2022-08-29 20:40:16 +1000

    fix: spelling
    Signed-off-by: Jim <compositr@compositr.dev>

--- src/common/handlers/upgradeToWS.ts
+++ src/common/handlers/upgradeToWS.ts
@@ -1,9 +1,9 @@
 import { RouteHandlerMethod } from "fastify";
 
-const hanlder: RouteHandlerMethod = async function upgradeToWS(req, res) {
+const handler: RouteHandlerMethod = async function upgradeToWS(req, res) {
   res
     .status(101)
     .header("Upgrade", "websocket")
     .header("Connection", "upgrade");
 };
-export default hanlder;
+export default handler;
