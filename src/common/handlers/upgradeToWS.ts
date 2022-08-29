01b365c9023f839ebc476649f1f925b33750164b
Jim <compositr@compositr.dev>
2022-08-29 20:59:01 +1000

    fix: this was not required
    Signed-off-by: Jim <compositr@compositr.dev>

--- src/common/handlers/upgradeToWS.ts
+++
@@ -1,9 +0,0 @@
-import { RouteHandlerMethod } from "fastify";
-
-const handler: RouteHandlerMethod = async function upgradeToWS(req, res) {
-  res
-    .status(101)
-    .header("Upgrade", "websocket")
-    .header("Connection", "upgrade");
-};
-export default handler;
