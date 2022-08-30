bfd42e9d3fc605f221ddee4cec105a87bbebeca6
Jim <compositr@compositr.dev>
2022-08-30 17:35:21 +1000

    feat: decorate fastify with wsMap
    Signed-off-by: Jim <compositr@compositr.dev>

---
+++ src/plugins/wsPlugin.ts
@@ -0,0 +1,17 @@
+import fp from "fastify-plugin";
+import { FastifyPluginAsync } from "fastify";
+import { PrismaClient } from "@prisma/client";
+import { WebSocket } from "ws";
+
+// Use TypeScript module augmentation to declare the type of server.prisma to be PrismaClient
+declare module "fastify" {
+  interface FastifyInstance {
+    wsSockets: Map<string, WebSocket>;
+  }
+}
+
+const wsPlugin: FastifyPluginAsync = fp(async (server, options) => {
+  server.decorate("wsSockets", new Map<string, WebSocket>());
+});
+
+export default wsPlugin;
