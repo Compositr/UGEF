693672b79eccd25714d01462891c628e852ec87e
Jim <compositr@compositr.dev>
2022-09-02 20:59:41 +1000

    fix: invite regex now contains dash
    Signed-off-by: Jim <compositr@compositr.dev>

--- src/schema/formats/Invite.ts
+++ src/schema/formats/Invite.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
  * @description Regex matching a Discord Invite
  * @since v0.0.1
  */
-export const UnwrappedInvite = z.string().regex(/^\w*$/);
+export const UnwrappedInvite = z.string().regex(/^[\w-]*$/);
 
 export default z.object({
   $invite: UnwrappedInvite,
